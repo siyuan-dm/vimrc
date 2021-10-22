ce81ad0b0f29d47375ee545a1a4987e4e36079bf
Siyuan Liu <tomato.sliu@gmail.com>
2021-10-23 00:01:23 +0800

    add rg

--- setup.sh
+++ setup.sh
@@ -11,3 +11,6 @@ vim +PlugInstall +qall
 git config --global diff.tool vimdiff
 git config --global difftool.prompt false
 git config --global alias.d difftool
+
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+sudo dpkg -i ripgrep_13.0.0_amd64.deb && rm ripgrep_13.0.0_amd64.deb
