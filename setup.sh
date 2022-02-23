d84015cb47ef5313bcf94582b80346fb15fbd1ae
Siyuan Liu <siyuan.liu@deepmirror.com>
2022-02-23 13:35:22 +0800

    update

--- setup.sh
+++ setup.sh
@@ -5,6 +5,11 @@ set -e
 # Install nodejs
 sudo curl -sL install-node.now.sh/lts | sudo bash
 
+# Install neovim
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/local/bin/nvim
+
 # Config vim
 bash install_awesome_vimrc.sh
 vim +PlugInstall +qall
@@ -16,3 +21,5 @@ curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_
 sudo dpkg -i ripgrep_13.0.0_amd64.deb && rm ripgrep_13.0.0_amd64.deb
 
 # Go install buildifier, buildifier is under ~/go/bin, add it to zshrc
+# Install YouCompleteMe
+#   cd ~/.vim/plugged/YouCompleteMe && ./install.py --clangd-completer
